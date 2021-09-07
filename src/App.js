45582aae41b259c068fbd9de267bbd280baf98f6
Dechen6 <88133017+Dechen6@users.noreply.github.com>
2021-09-07 14:11:29 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Home from './components/pages/Home';
+import Header from './components/pages/Header';
+import Footer from './components/pages/Footer';
+import './components/pages/style.css'
+
+
+
+function App() {
+  return (
+    <div>
+    <Header/>
+    <Home/>
+    <Footer/>
+    </div>
+    
+  );
+}
+
+export default App;
